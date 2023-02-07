94d1b19d1537ab9e7def95bb32ad201f7da44102
Iyanu <iyanuajimobi12@gmail.com>
2023-02-07 14:10:34 -0800

    andded suport for keyword arguments

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-
+Contains BaseModel class.
 """
 import uuid
 from datetime import datetime
@@ -10,7 +10,7 @@ import models
 class BaseModel:
     """ Base model for AirBnB package"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ 
         Initialization methon to initilize each instance of BaseModel
         Args:
@@ -20,9 +20,17 @@ class BaseModel:
             created_at: Time the each instance was created
             updated_at: Last time the instance was updated
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        if len(kwargs):
+            iso_date = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    self.__dict__[key] = datetime.strptime(value, iso_date)
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
 
     def save(self):
         """ Updates instance attribute *updated_at* with current datetime """
@@ -40,4 +48,4 @@ class BaseModel:
     def __str__(self):
         """ String Representation of base model"""
         return "[{}] ({}) {}".format(self.__class__.__name__, \
-                self.id, self.__dict__)
+            self.id, self.__dict__)
---
+++ models/engine/file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+"""
+import json
+import models
+
+
+class FileStorage:
+    """ """
+    
