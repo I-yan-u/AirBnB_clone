52c8547e49bfd5bb6b8c324ca2ce66d2e4e467dc
Almadny <Almadny2u@gmail.com>
2023-02-13 01:52:17 -0800

    Amends

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,13 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ Initializes all instance attributes """
-        #if len(kwargs) == 0:
+        # if len(kwargs) == 0:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-            #models.storage.new(self)
+            # models.storage.new(self)
 
-        #else:
+        # else:
            # self.id = kwargs['id']
            # self.created_at = datetime.fromisoformat(kwargs['created_at'])
            # self.updated_at = datetime.fromisoformat(kwargs['created_at'])
@@ -28,7 +28,7 @@ class BaseModel:
     def save(self):
         """Updates the updated_at instance variable"""
         self.updated_at = datetime.now()
-        #models.storage.save()
+        # models.storage.save()
 
     def to_dict(self):
         """ Returns a copy of __dict__"""
