19e7af67e55b5c444d97810c5bd227c233a691be
Almadny <Almadny2u@gmail.com>
2023-02-13 02:07:43 -0800

    Amends

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """ Module that contains the Basemodel for the project"""
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
@@ -13,12 +12,6 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-            # models.storage.new(self)
-
-        # else:
-           # self.id = kwargs['id']
-           # self.created_at = datetime.fromisoformat(kwargs['created_at'])
-           # self.updated_at = datetime.fromisoformat(kwargs['created_at'])
 
     def __str__(self):
         """ Print the representation of the object """
