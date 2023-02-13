7e8eed4fa7ef77ed4465ba1e55719d25cbc66053
Almadny <Almadny2u@gmail.com>
2023-02-13 02:27:57 -0800

    Edited init def

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class BaseModel:
     """ Base Model Class """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """ Initializes all instance attributes """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
