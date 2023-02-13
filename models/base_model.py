8739f59f3da241f681e6b9dabb7b558891050f9e
Almadny <Almadny2u@gmail.com>
2023-02-13 01:20:09 -0800

    Re

--- models/base_model.py
+++ models/base_model.py
@@ -10,16 +10,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ Initializes all instance attributes """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        #if len(kwargs) == 0:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
             #models.storage.new(self)
 
-        else:
-            self.id = kwargs['id']
-            self.created_at = datetime.fromisoformat(kwargs['created_at'])
-            self.updated_at = datetime.fromisoformat(kwargs['created_at'])
+        #else:
+           # self.id = kwargs['id']
+           # self.created_at = datetime.fromisoformat(kwargs['created_at'])
+           # self.updated_at = datetime.fromisoformat(kwargs['created_at'])
 
     def __str__(self):
         """ Print the representation of the object """
